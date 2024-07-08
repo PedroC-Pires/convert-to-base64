947c25ca75a829a8d5d5a1f070ab3639a4aa478b
PEDRO DA CONCEIÇÃO PIRES <pedro_c_pires@estudante.sesisenai.org.br>
2024-07-08 08:03:07 -0300

    1.0

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 /*
 
-    onclick(base64(this))
-    onchange(base64(this))
+    --Quick guide--
 
+    --Two ways to use it on HTML--
+    onclick(base64(this));
+    onchange(base64(this));
+
+    --Two ways to get the value on JS--
+    console.log(base64Element);
+    let a = base64Element;
+    
 */
 
 const imageId = document.getElementById("image");
-let a = null;
+let base64Element = null;
 function base64(element) {
     var file = element.files[0];
     var reader = new FileReader();
     reader.onloadend = function () {
         //console.log(reader.result)
-        a = reader.result;
+        base64Element = reader.result;
     };
     reader.readAsDataURL(file);
 }
